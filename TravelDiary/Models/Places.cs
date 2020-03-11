e894acbd57be65a5e3d63bb0ac28e9c35cb5b054
Geoff Goetz and Michelle Morin <michelle@>
2020-03-11 09:36:36 -0700

    add logic to pass fifth test

--- TravelDiary/Models/Places.cs
+++ TravelDiary/Models/Places.cs
@@ -5,12 +5,14 @@ namespace TravelDiary.Models
   public class Place
   {
     public string CityName { get; }
+    public int Id { get; }
     private static List<Place> _allPlaces { get; } = new List<Place> {};
 
     public Place(string cityName)
     {
       CityName = cityName;
       _allPlaces.Add(this);
+      Id = _allPlaces.Count;
     }
 
     public static List<Place> GetAll()
