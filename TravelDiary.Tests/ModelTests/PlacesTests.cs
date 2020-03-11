6d4a47907f73ac33281de836f00878b4bb3714b0
Geoff Goetz and Michelle Morin <geoff@>
2020-03-11 09:15:12 -0700

    add second test, fail

--- TravelDiary.Tests/ModelTests/PlacesTests.cs
+++ TravelDiary.Tests/ModelTests/PlacesTests.cs
@@ -11,8 +11,22 @@ namespace TravelDiary.Tests
     [TestMethod]
     public void PlaceConstructor_CreatesInstanceOfPlace_Place()
     {
-      Place newPlace = new Place("Istanbul");
+      Place newPlace = new Place("test");
       Assert.AreEqual(typeof(Place), newPlace.GetType());
+    }
+
+    [TestMethod]
+    public void GetCityName_ReturnsCityName_String()
+    {
+      // Arrange
+      string cityName = "Istanbul";
+
+      // Act
+      Place newPlace = new Place(cityName);
+      string result = newPlace.CityName;
+
+      // Assert
+      Assert.AreEqual(cityName, result);
     }
   }
 }
